60b6e846a4c5eb155793dba8593162e0913e1c05
GreenIsaiah <ikragland@my.waketech.edu>
2026-05-29 13:22:56 -0400

    Worked on fire/smoke effect and camera movement
    I improved on the  fire/smoke effect and started with the camera movement (it moves with the player but doesnt flower the player

--- Assets/Scripts/charcontrol.cs
+++ Assets/Scripts/charcontrol.cs
@@ -13,14 +13,17 @@ public class charcontrol : MonoBehaviour
     public Transform cameraTransform;
     ThridPersonCamera cam;
     //camera
+
     [Header("Input Actions")]
     public InputActionReference moveAction; // expects Vector2
 
     private void Awake()
     {
+        /*
         //camera
         cam = Camera.main.GetComponent<ThridPersonCamera>();
         //camera
+        */
         controller = gameObject.AddComponent<CharacterController>();
     }
 
@@ -37,7 +40,6 @@ public class charcontrol : MonoBehaviour
     void Update()
     {
         //camera
-        /*
         Vector2 input = moveAction.action.ReadValue<Vector2>();
         Vector3 camFordward = cameraTransform.forward;
         Vector3 camRight = cameraTransform.right;
@@ -50,12 +52,11 @@ public class charcontrol : MonoBehaviour
 
         Vector3 move = camFordward * input.y + camRight * input.x;
         move = Vector3.ClampMagnitude(move, 1f);
-        */
         //camera
 
         // Read input
-        Vector2 input = moveAction.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
+        //added Vector2 input = moveAction.action.ReadValue<Vector2>();
+        //added Vector3 move = new Vector3(input.x, 0, input.y);
         move = Vector3.ClampMagnitude(move, 1f);
 
         if (move != Vector3.zero)
