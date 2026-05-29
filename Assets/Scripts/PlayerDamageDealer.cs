dadc9f3c5cb2ef1a8cd2c3493ec1eb5f1772b7d3
devinjj9 <149423070+devinjj9@users.noreply.github.com>
2026-05-29 17:37:34 -0400

    Add HP bar for player

---
+++ Assets/Scripts/PlayerDamageDealer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerDamageDealer : MonoBehaviour
+{
+    public float damage = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+        }
+    }
+}
