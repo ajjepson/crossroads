93d5d052205ee015c93d56b25504e37f334078f7
GreenIsaiah <ikragland@my.waketech.edu>
2026-05-30 13:33:05 -0400

    added 2 heath bars 1 for knight 1 for archer
    added 2 health bars scripts for archer and knight

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public Slider healthBar;
+    public TMP_Text playerHealth;
+    public int health = 150;
+    public int maxHealth = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth.text = health + " / " + maxHealth;
+        healthBar.value = (float)health / (float)maxHealth;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            //you take damage
+            if (health > 0)
+            {
+                Debug.Log("you took damage");
+                health = health - 10;
+            }
+        }
+    }
+}
