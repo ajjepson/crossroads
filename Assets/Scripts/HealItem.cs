91b247cb4614581478e7155ccdc537039af90a1b
GreenIsaiah <ikragland@my.waketech.edu>
2026-05-31 21:39:06 -0400

    added a breakable object with a chance to drop heal
    I added a breakable object with a chance to drop  a healing item

---
+++ Assets/Scripts/HealItem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public class HealItem : MonoBehaviour
+{
+    public GameObject healthPickup;
+    public SwordSwing SwordSwing;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (SwordSwing.attacking == true)
+        {
+            if (UnityEngine.Random.value < 0.5f)
+            {
+                if (healthPickup != null)
+                {
+                    Instantiate(healthPickup, transform.position, Quaternion.identity);
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+}
