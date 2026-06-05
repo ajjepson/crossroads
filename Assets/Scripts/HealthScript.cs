ce91b5a32108f9d687bc26daf2bc2790914cb2a8
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-05 11:45:24 -0400

    updated all 4  UI cool downs
    my IsaiahTest i made all 4 cooldowns

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
 
 public class HealthScript : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class HealthScript : MonoBehaviour
     public int health = 150;
     public int maxHealth = 0;
     public bool sheildActive = false;
+    //new
+    //public float sheildStreanth = 2f;
+    public float sheildLength = 3f;
+    public float sheildNotActive = 4f;
+    public bool canPlayerSheild = true;
+    public Image sheildImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,26 +28,55 @@ public class HealthScript : MonoBehaviour
         //Debug.Log(sheildActive);
         playerHealth.text = health + " / " + maxHealth;
         healthBar.value = (float)health / (float)maxHealth;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canPlayerSheild == true)
         {
+            Debug.Log("holding sheild");
+            StartCoroutine(SheildBlock());
+            /*
             if (sheildActive == false)
             {
                 sheildActive = true;
-                Invoke("coolDown", 1);
+                //Invoke("coolDown", 1);
             }
+            */
         }
     }
-    public void coolDown()
+    private IEnumerator SheildBlock()
     {
+        canPlayerSheild = false;
+        sheildActive = true;
+        //yield return new WaitForSeconds(sheildLength);
+
+        float countUpTime = 0f;
+        sheildImage.fillAmount = 1f;
+
+        while (countUpTime < sheildLength)
+        {
+            countUpTime += Time.deltaTime;
+            sheildImage.fillAmount = 1f - (countUpTime / sheildLength);
+            yield return null;
+        }
         sheildActive = false;
+        //flip this to fill back up (replace countUpTime with countDownTime and sheildLength with sheildNotActive)
+        float countDownTime = 0f;
+        sheildImage.fillAmount = 0f;
+        while (countDownTime < sheildNotActive)
+        {
+            countDownTime += Time.deltaTime;
+            sheildImage.fillAmount = (countDownTime / sheildNotActive);
+            yield return null;
+        }
+        sheildImage.fillAmount = 1f;
+        canPlayerSheild = true;
     }
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("enemy"))
         {
-            if (sheildActive == true)
+            if (sheildActive)
             {
                 //take no damage
+                return;
             }
             //you take damage
             if (health > 0)
