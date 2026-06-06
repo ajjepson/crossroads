8328fcb15f3d3691cec9331e675b1a5ef9e30299
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-06 18:51:05 -0400

    added a navMesh and script for the spider boss
    added a navMesh and script for the spider boss following the player

--- Assets/Scripts/SpiderBoss.cs
+++ Assets/Scripts/SpiderBoss.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class SpiderBoss : MonoBehaviour
@@ -9,10 +10,35 @@ public class SpiderBoss : MonoBehaviour
     private int spiderHealth = 500;
     public int spiderMaxHealth = 0;
     public SwordSwing swordSwing;
+
+    //new
+    enum EnemyAIActions { walking, chasing, attacking }
+
+    private EnemyAIActions currentState;
+    private NavMeshAgent agent;
+
+    [SerializeField] private Transform[] checkpoints;
+    private int currentCheckPointIndex;
+
+    private GameObject player;
+    private float distanacePlayer;
+
+    private const float distanaceAttack = 2f;
+    private const float distanaceFollowing = 5.0f;
+    //new
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spiderMaxHealth = spiderHealth;
+        //new
+        swordSwing.canPlayerSwing = false;
+
+        currentState = EnemyAIActions.walking;
+        agent = GetComponent<NavMeshAgent>();
+        currentCheckPointIndex = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //new
     }
 
     // Update is called once per frame
@@ -20,7 +46,59 @@ public class SpiderBoss : MonoBehaviour
     {
         spiderHealthText.text = spiderHealth + " / " + spiderMaxHealth;
         spiderhealthBar.value = (float)spiderHealth / (float)spiderMaxHealth;
+
+        //new
+        if (player == null)
+        {
+            return;
+        }
+        DetermineCurrentState();
+        FollowingCurrentState();
+        //new
     }
+    //new
+    private void DetermineCurrentState()
+    {
+        distanacePlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanacePlayer < distanaceAttack)
+        {
+            currentState = EnemyAIActions.attacking;
+        }
+        else if (distanacePlayer < distanaceFollowing)
+        {
+            currentState = EnemyAIActions.chasing;
+        }
+        else
+        {
+            currentState = EnemyAIActions.walking;
+        }
+    }
+    private void FollowingCurrentState()
+    {
+        switch (currentState)
+        {
+            case EnemyAIActions.walking:
+
+                if (!agent.pathPending && agent.remainingDistance < 1.0f)
+                {
+                    //close to player
+                    agent.SetDestination(checkpoints[currentCheckPointIndex].position);
+                    currentCheckPointIndex++;
+                    if (currentCheckPointIndex >= checkpoints.Length)
+                    {
+                        currentCheckPointIndex = 0;
+                    }
+                }
+                break;
+            case EnemyAIActions.chasing:
+                agent.SetDestination(player.transform.position);
+                break;
+            case EnemyAIActions.attacking:
+                agent.SetDestination(player.transform.position);
+                break;
+        }
+    }
+    //new
     public void OnTriggerEnter(Collider other)
     {
         if (swordSwing.canPlayerSwing == true)
