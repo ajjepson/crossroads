93c6c9f051a80886f452667443ae9f474801c305
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-07 17:53:13 -0400

    made switch statement to load the levels on a ONtrigger
    made switch statement to load the levels on a ONtrigger

--- Assets/Scripts/changingTestLevels.cs
+++ Assets/Scripts/changingTestLevels.cs
@@ -4,10 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class changingTestLevels : MonoBehaviour
 {
+    private string loadCertainScene;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+       loadCertainScene = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
@@ -20,11 +21,67 @@ public class changingTestLevels : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("IsaiahTest2");
+            switch (loadCertainScene)
+            {
+                case "IsaiahTest":
+                    SceneManager.LoadScene("IsaiahTest2");
+                    break;
+                case "IsaiahTest2":
+                    SceneManager.LoadScene("1-1 Village");
+                    break;
+                case "1-1 Village":
+                    SceneManager.LoadScene("1-2 Meadows");
+                    break;
+                case "1-2 Meadows":
+                    SceneManager.LoadScene("1-3 Cave");
+                    break;
+                case "1-3 Cave":
+                    SceneManager.LoadScene("2-1");
+                    break;
+                case "2-1":
+                    SceneManager.LoadScene("2-2");
+                    break;
+                case "2-2":
+                    SceneManager.LoadScene("2-3");
+                    break;
+                case "2-3":
+                    SceneManager.LoadScene("end");
+                    break;
+            }
+        }
+        else if (other.CompareTag("archer"))
+        {
+            switch (loadCertainScene)
+            {
+                case "IsaiahTest":
+                    SceneManager.LoadScene("IsaiahTest2");
+                    break;
+                case "IsaiahTest2":
+                    SceneManager.LoadScene("1-1 Village");
+                    break;
+                case "1-1 Village":
+                    SceneManager.LoadScene("1-2 Meadows");
+                    break;
+                case "1-2 Meadows":
+                    SceneManager.LoadScene("1-3 Cave");
+                    break;
+                case "1-3 Cave":
+                    SceneManager.LoadScene("2-1");
+                    break;
+                case "2-1":
+                    SceneManager.LoadScene("2-2");
+                    break;
+                case "2-2":
+                    SceneManager.LoadScene("2-3");
+                    break;
+                case "2-3":
+                    SceneManager.LoadScene("end");
+                    break;
+            }
         }
         else
         {
-            SceneManager.LoadScene("MenuScreen");
+            //none player touch sceneloader
         }
     }
 }
