e42d47a8e4d5bc794237fb205ffff3959375174e
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-08 16:01:29 -0400

    added the 2 tree fall animation
    added the 2 tree fall animation all in tree fall script

--- Assets/Scripts/TreeFallScript.cs
+++ Assets/Scripts/TreeFallScript.cs
@@ -4,21 +4,48 @@ public class TreeFallScript : MonoBehaviour
 {
     public GameObject hiddenBox;
     private Animator animator;
+    private Animator animator2;
+    private Animator animator3;
+    private int treefellcount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hiddenBox.SetActive(false);
         animator = GetComponent<Animator>();
+        animator2 = GetComponent<Animator>();
+        animator3 = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void OnTriggerEnter(Collider other)
+    public void FirstTreeFall()
     {
         hiddenBox.SetActive(true);
         animator.SetTrigger("TreeFall");
+    }
+    public void SecondTreeFall()
+    {
+        animator.SetTrigger("Treefell1");
+        treefellcount += 1;
+    }
+    public void ThridTreeFall()
+    {
+        animator.SetTrigger("TreeFell2");
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FirstTreeFall();
+        }
+        if (other.CompareTag("archer"))
+        {
+            SecondTreeFall();
+            if (treefellcount >= 1)
+            {
+                ThridTreeFall();
+            }
+
+            //TreeFell2
+        }
+        //hiddenBox.SetActive(true);
+        //animator.SetTrigger("TreeFall");
     }
 }
