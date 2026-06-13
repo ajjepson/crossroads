f2454931e49fc93917ddf7168502b1390c8357aa
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-13 13:17:12 -0400

    Started adding in some audio
    Started adding in some audio: audio added walking, sword attack, arrow fired, theme.  Added but not in death audio button clicking audio

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip buttonClickAudio;
+    private AudioSource audioButtonSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioButtonSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,23 +19,33 @@ public class UiManager : MonoBehaviour
     }
     public void PlayButton()
     {
+        audioButtonSource.clip = buttonClickAudio;
+        audioButtonSource.Play();
         SceneManager.LoadScene("1-1 Village");
     }
     public void HelpButton()
     {
+        audioButtonSource.clip = buttonClickAudio;
+        audioButtonSource.Play();
         // Changed it to Isaiah Test SceneManager.LoadScene("HelpScreen");
         SceneManager.LoadScene("IsaiahTest");
     }
     public void MenuButton()
     {
+        audioButtonSource.clip = buttonClickAudio;
+        audioButtonSource.Play();
         SceneManager.LoadScene("MenuScreen");
     }
     public void CreditsButton()
     {
+        audioButtonSource.clip = buttonClickAudio;
+        audioButtonSource.Play();
         SceneManager.LoadScene("Credits");
     }
     public void QuitButton()
     {
+        audioButtonSource.clip = buttonClickAudio;
+        audioButtonSource.Play();
         Application.Quit();
         Debug.Log("Quit ButtonPressed");
     }
