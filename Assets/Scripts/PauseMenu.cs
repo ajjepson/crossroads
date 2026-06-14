7b828d590b584feed8c7086f44f38b08981c26f1
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-14 15:54:42 -0400

    added volume controls
    added volume controls (im testing this one out)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,10 +5,22 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pausedMenu;
 
+    public GameObject playButton;
+    public GameObject mainMenuButton;
+    public GameObject resetButton;
+    public GameObject quitButton;
+    public GameObject backButton;
+    public GameObject volumeButton;
+    public GameObject pauseText;
+    public GameObject volumeText;
+    public GameObject volumeSlider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pausedMenu.SetActive(false);
+        volumeSlider.SetActive(false);
+        volumeText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,6 +46,30 @@ public class PauseMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
+    }
+    public void VolumeButton()
+    {
+        playButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+        resetButton.SetActive(false);
+        quitButton.SetActive(false);
+        backButton.SetActive(true);
+        volumeText.SetActive(true);
+        pauseText.SetActive(false);
+        volumeButton.SetActive(false);
+        volumeSlider.SetActive(true);
+    }
+    public void BackButton()
+    {
+        playButton.SetActive(true);
+        mainMenuButton.SetActive(true);
+        resetButton.SetActive(true);
+        quitButton.SetActive(true);
+        backButton.SetActive(false);
+        volumeText.SetActive(false);
+        pauseText.SetActive(true);
+        volumeButton.SetActive(true);
+        volumeSlider.SetActive(false);
     }
     public void QuitButton()
     {
