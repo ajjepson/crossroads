f93c11d3e27e33e8b78b63512f8b8c1655f610c8
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-15 13:40:00 -0400

    added in eating and hurt and archer dash audio
    added in eating and hurt audio and dash

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,7 +5,7 @@ public class SoundManager : MonoBehaviour
 {
     public Slider volumeSlider;
     //public Slider othersSlider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //this script manaages the slider for controlling the volume
     void Start()
     {
         if (PlayerPrefs.HasKey("soundVolume"))
