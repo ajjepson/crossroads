33f4b5d33435acc2af6494ecbc8ee3f42797c817
devinjj9 <149423070+devinjj9@users.noreply.github.com>
2026-06-16 15:19:49 -0400

    Fix the enemy spawners/add coding animations

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,126 +4,47 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [Header("Interaction")]
-    public GameObject interactText;
-    private bool playerInRange;
-
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject[] enemyPrefabs;
 
     [Header("Spawn Settings")]
-    [SerializeField] private int maxEnemies = 5;
     [SerializeField] private float spawnInterval = 3f;
     [SerializeField] private Vector3 areaSize = new Vector3(10f, 0f, 10f);
-    public int currentEnemies;
 
-    [Header("Spawner HP System")]
-    public int spawnerHP = 2;
-    public int maxSpawnerHP = 2;
+    [Header("Wave Settings")]
+    [SerializeField] private int totalEnemiesToSpawn = 20;
 
-    private bool canPress = true;
-    public float pressCooldown = 1.5f;
-
-    [Header("UI")]
-    public FloatingHealthBar healthBar;
+    private int enemiesSpawned = 0;
 
     void Start()
     {
         StartCoroutine(SpawnEnemyLoop());
-
-        if (interactText != null)
-            interactText.SetActive(false);
-
-        if (healthBar != null)
-        {
-            healthBar.SetTarget(transform);
-            healthBar.UpdateBar(spawnerHP, maxSpawnerHP);
-        }
-    }
-
-    void Update()
-    {
-        if (playerInRange && Input.GetKeyDown(KeyCode.E) && canPress)
-        {
-            StartCoroutine(HandlePress());
-        }
-    }
-
-    IEnumerator HandlePress()
-    {
-        canPress = false;
-
-        spawnerHP--;
-
-        Debug.Log("Spawner hit! HP left: " + spawnerHP);
-
-        
-        if (healthBar != null)
-        {
-            healthBar.UpdateBar(spawnerHP, maxSpawnerHP);
-        }
-
-        if (spawnerHP <= 0)
-        {
-            DestroySpawner();
-            yield break;
-        }
-
-        yield return new WaitForSeconds(pressCooldown);
-        canPress = true;
-    }
-
-    void DestroySpawner()
-    {
-        StopAllCoroutines();
-        Destroy(gameObject);
     }
 
     IEnumerator SpawnEnemyLoop()
     {
-        while (true)
+        while (enemiesSpawned < totalEnemiesToSpawn)
         {
-            if (currentEnemies < maxEnemies)
-            {
-                SpawnEnemy();
-            }
+            SpawnEnemy();
+            enemiesSpawned++;
 
             yield return new WaitForSeconds(spawnInterval);
         }
+
+        Debug.Log("Spawner finished spawning enemies.");
+        Destroy(gameObject);
     }
 
     void SpawnEnemy()
     {
         Vector3 randomPos = transform.position + new Vector3(
-            Random.Range(-areaSize.x / 2, areaSize.x / 2),
-            0,
-            Random.Range(-areaSize.z / 2, areaSize.z / 2)
-        );
+        Random.Range(-areaSize.x / 2, areaSize.x / 2),
+        0,
+        Random.Range(-areaSize.z / 2, areaSize.z / 2));
 
-        Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        GameObject enemyToSpawn =
+            enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
 
-        currentEnemies++;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("archer"))
-        {
-            playerInRange = true;
-
-            if (interactText != null)
-                interactText.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("archer"))
-        {
-            playerInRange = false;
-
-            if (interactText != null)
-                interactText.SetActive(false);
-        }
+        Instantiate(enemyToSpawn, randomPos, Quaternion.identity);
     }
 
     private void OnDrawGizmos()
