3f565945a4475ce1699d28bd4a537de9adeb518e
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-16 14:27:51 -0400

    started working on spider spit
    started working on spider spit (spider spits a protectile at player)

--- Assets/Scripts/SpiderWebSpit.cs
+++ Assets/Scripts/SpiderWebSpit.cs
@@ -4,14 +4,26 @@ public class SpiderWebSpit : MonoBehaviour
 {
     public float webSpitSpeed = 30;
     private Rigidbody rigidbod;
+    private Transform chasePlayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidbod = GetComponent<Rigidbody>();
-        rigidbod.angularVelocity = transform.forward * webSpitSpeed;
+        //rigidbod.angularVelocity = transform.forward * webSpitSpeed;
+        chasePlayer = GameObject.FindGameObjectWithTag("Player").transform;
         Destroy(gameObject, 7f);
 
 
+    }
+    private void Update()
+    {
+        if (chasePlayer != null)
+        {
+            //player cant be found
+            return;
+        }
+        Vector3 playerDirection = (chasePlayer.position - transform.position).normalized;
+        transform.Translate(Vector3.forward * webSpitSpeed * Time.deltaTime);
     }
 
     public void OnTriggerEnter(Collider other)
