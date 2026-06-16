25b62ccccac1e3dfb82467e989d6c2a04bea42ec
GreenIsaiah <ikragland@my.waketech.edu>
2026-06-15 21:05:07 -0400

    started working on spider boss
    started working on spider boss

--- Assets/Scripts/SpiderBoss.cs
+++ Assets/Scripts/SpiderBoss.cs
@@ -12,8 +12,19 @@ public class SpiderBoss : MonoBehaviour
     public int spiderMaxHealth = 0;
     public SwordSwing swordSwing;
 
+    public GameObject poisionBreath;
+
+    //for poisionBreathChance not repeating per frame
+    private bool breathAttack = false;
+
+    //for runing away
+    private bool didSpiderRan = false;
+    private float cooldownSpider = 0;
+    private float retreatTime = 3;
+    private float retreatLength = 10;
+
     //new
-    enum EnemyAIActions { walking, chasing, attacking }
+    enum EnemyAIActions { walking, chasing, attacking, /*new*/ runaway }
 
     private EnemyAIActions currentState;
     private NavMeshAgent agent;
@@ -30,6 +41,7 @@ public class SpiderBoss : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        poisionBreath.SetActive(false);
         spiderMaxHealth = spiderHealth;
         //new
         //swordSwing.canPlayerSwing = false;
@@ -62,6 +74,53 @@ public class SpiderBoss : MonoBehaviour
         FollowingCurrentState();
         //new
     }
+
+    //handle spider retreat
+    public void SpiderTookDamage(int amount)
+    {
+        spiderHealth -= amount;
+        //spider chance of running away when hp is 150 or more is 25%
+        if (spiderHealth >=150 && !didSpiderRan && UnityEngine.Random.value <= .25f)
+        {
+            SpiderBossRan();
+        }
+        //spider chance of running away when hp is 150-100 is 50%
+        else if (spiderHealth >= 100 && spiderHealth < 150 && !didSpiderRan && UnityEngine.Random.value <= .5f)
+        {
+            SpiderBossRan();
+        }
+        //spider chance of running away when hp is 100 or less is 75%
+        else if (spiderHealth < 100 && !didSpiderRan && UnityEngine.Random.value <= .75f)
+        {
+            SpiderBossRan();
+        }
+        else
+        {
+            //spider did not run
+        }
+    }
+    private void SpiderBossRan()
+    {
+        didSpiderRan = true;
+        currentState = EnemyAIActions.runaway;
+        cooldownSpider = retreatTime;
+
+    }
+    private void SpiderGettingAway()
+    {
+        cooldownSpider -= Time.deltaTime;
+        Vector3 direction = (transform.position - player.transform.position).normalized;
+        Vector3 targetPath = transform.position + direction * retreatLength;
+        agent.SetDestination(targetPath);
+
+        if (cooldownSpider <= 0)
+        {
+            didSpiderRan = false;
+            currentState = EnemyAIActions.walking;
+        }
+    }
+    //handle spider retreat
+
     //new
     private void DetermineCurrentState()
     {
@@ -69,14 +128,33 @@ public class SpiderBoss : MonoBehaviour
         if (distanacePlayer < distanaceAttack)
         {
             currentState = EnemyAIActions.attacking;
+            poisionBreath.SetActive(false);
+            breathAttack = false;
         }
         else if (distanacePlayer < distanaceFollowing)
         {
             currentState = EnemyAIActions.chasing;
+            if (!breathAttack)
+            {
+                breathAttack = true;
+                //chance of poision attack is 25% 
+                if (UnityEngine.Random.value < .25f)
+                {
+                    poisionBreath.SetActive(true);
+                }
+                else
+                {
+                    //poison chance failed
+                    poisionBreath.SetActive(false);
+
+                }
+            }
         }
         else
         {
             currentState = EnemyAIActions.walking;
+            poisionBreath.SetActive(false);
+            breathAttack = false;
         }
     }
     private void FollowingCurrentState()
@@ -102,6 +180,9 @@ public class SpiderBoss : MonoBehaviour
             case EnemyAIActions.attacking:
                 agent.SetDestination(player.transform.position);
                 break;
+                case EnemyAIActions.runaway:
+                SpiderGettingAway();
+                    break;
         }
     }
     //new
